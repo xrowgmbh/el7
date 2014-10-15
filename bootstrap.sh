0fca8dbf475ea0e7d92370d74d200d241f9e030e
xrow <bjoern@xrow.de>
2014-10-15 04:41:44 +0200

    - save

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,7 @@ yum -y groupinstall Base
 #yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 yum -y install https://anorien.csc.warwick.ac.uk/mirrors/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 yum -y install ansible
-cp /etc/ansible/hosts /etc/ansible/hosts.dist
 
 cat <<EOL > /etc/ansible/hosts
-localhost
 
-[webservers]
-localhost
 EOL
